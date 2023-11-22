9c0c4a4d821a0e1bdbf3e9d75d6c52d85cab4bec
Lucas Miralha <101230062+LucasMiralha@users.noreply.github.com>
2023-11-22 13:35:21 -0300

    Update Main.cpp
    Adição de comentários extensivos e remoção de uma biblioteca não utilizada.

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <string>
 #include "BubbleSort.cpp"
 #include "MergeSort.cpp"
 #include "QuickSort.cpp"
-
-//#include <pqxx/pqxx>
 
 using namespace std;
 
@@ -17,38 +14,57 @@ int main()
 	vector<float> dados;
 	ifstream nums;
 	ofstream results;
-	nums.open("num10k.txt", ios::in);
+	//nomes dos arquivos e seus dados
+	//num10k = 10 mil numeros float com 2 casas decimais aleatorios entre 0 e 1
+	//num100k = 100 mil numeros float com 2 casas decimais aleatorios entre 0 e 1
+	//num1kk = 1 milhao de numeros float com 2 casas decimais aleatorios entre 0 e 1
+	nums.open("num100k.txt", ios::in);
 	
 	float x;
-	
+	//Leitura de dados do arquivo .txt
 	while (1) 
-	{         
-        nums >> x;              
-        if(nums.eof())  
+	{       
+		//Alocando o numero do arquivo
+        	nums >> x;              
+		//Se tiver chegado no fim do arquivo ocorre um break antes de 
+		//colocar o numero no vector de fato, para nao ocorrer de alocar 
+		//um numero nao existente no arquivo dentro do vector
+        	if(nums.eof())  
 		{        
-            break;                                
-	    }
-	    dados.push_back(x);
+            		break;                                
+	    	}
+		//Alimentado o vector com os numeros do arquivo
+		dados.push_back(x);
 	}
-	
+	//Coleta da quantidade de valores do arquivo
 	int n = dados.size();
+	//Criacao dos arrays que vao armazenar os numeros do arquivo
+	//Isso e feito para poder usa-los nos algoritmos de sort
 	float arr[n], arr1[n], arr2[n];
 	
-    for (int i = 0; i < n; i++) 
+    	for (int i = 0; i < n; i++) 
 	{
-        arr[i] = dados[i];
-        arr1[i] = dados[i];
-        arr2[i] = dados[i];
-    }
-    
+		//Passando o vector para 3 arrays diferentes
+		//Sao usados 3 arrays iguais para manter a mesma
+		//desordem em todos os algoritmos de sort
+	        arr[i] = dados[i];
+	        arr1[i] = dados[i];
+	        arr2[i] = dados[i];
+    	}
+
+	//Criacao ou adicao dos resultados em um arquivo .csv
 	results.open("Resultados_10k_intel5_11G_16GBRAM.csv", ios::out | ios::app);
-	
+
+	//Quantidade de iteracoes, basicamente o tamanho dos dados
+	//Foi escolhido 10 mil como populacao e posteriormente mil como amostra
+	//Com 100 mil dados cada iteracao dura em media 35 segundos, entao esse codigo de exemplo
+	//demora em media 1 hora e meia para gerar o csv
 	int iterations = 10000;
 	for (int iterations = 0; iterations < n; iterations++) 
 	{
-		cout << mergeSort(arr2, 0, n-1) << endl;
-		//results << bubbleSort(arr, n) << ", " << quickSort(arr1, 0, n-1) << ", " << mergeSort(arr2, 0, n-1) << "\n";
-    }
+		//Armazenamento de todos os dados no csv
+		results << bubbleSort(arr, n) << ", " << quickSort(arr1, 0, n-1) << ", " << mergeSort(arr2, 0, n-1) << "\n";
+    	}
 	
 	return 0;
 }
